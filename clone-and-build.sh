efbc2875494df97c0a6fa6d92544252606d5be31
Joona Romppanen <joona.romppanen@gmail.com>
2019-01-07 03:11:27 +0200

    Source build vars from vars file in target repo

--- clone-and-build.sh
+++ clone-and-build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 git clone $1 .
+source build_vars
 /bin/sh -c "./build.sh  -N \"${ARCHISO_NAME}\" -V \"${ARCHISO_VERSION}\" -L \"${ARCHISO_LABEL}\" -P \"${ARCHISO_PUBLISHER}\" -A \"${ARCHISO_APPLICATION_NAME}\" -o \"${ARCHISO_OUTPUT_DIRECTORY}\""
